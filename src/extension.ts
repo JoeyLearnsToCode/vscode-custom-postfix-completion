6fcbee9435f2304d9736b7e2d6520a5473a289e0
JoeyLearnsToCode <zilin.wang.1024@gmail.com>
2023-12-28 16:04:31 +0800

    基本完成 1. 添加调试模式配置 2. 把配置校验、模板解析、变量校验解析提前到配置刷新、初始化中，提高应用模板时性能

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,37 @@
 import * as vscode from 'vscode';
 
+const EXTENSION_NAME = "Custom Postfix Completion"
+const DEFAULT_WORD_REGEX = /\w+/
+const POSSIBLE_VARIABLE_REGEX = /\$\{[^\}]*\}/g;
+const VARIABLE_REGEX = /\$\{(\w+)(?:#(\d+))?(?::(\w+\(expr\))?(?::([^\}]+))?)?\}/g;
+let extensionContext: vscode.ExtensionContext;
+let isDebugMode = false;
+let configuration: vscode.WorkspaceConfiguration;
+let languagePostfixTemplatesMap: Map<string, LanguagePostfixTemplate>;
+type LanguagePostfixTemplate = {
+	triggerWord: string;
+	description: string;
+	exprRegExp: RegExp;
+	body: string[];
+
+	parsedBody: (string | TemplateVarible)[];
+}
+type TemplateVarible = {
+	name: string;
+	no?: number;
+	// 表达式和参数，如果不是 undefined，首个元素是表达式（函数）
+	expressionAndParam?: (((word: string) => string) | string)[];
+	defaultValue?: string;
+}
+
 export function deactivate() { }
 export function activate(context: vscode.ExtensionContext) {
-	// 初始计划配置
+	extensionContext = context;
+	// 初始化配置
 	try {
 		refreshConfigs();
 	} catch (error) {
-		console.log(error);
+		debugLog(error);
 		showErrorMessage('Fail to initialize, you can check console (in developer tools) to see more details');
 	}
 
@@ -23,6 +48,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 }
+
 function tryCommand(callback: (...args: any[]) => any): (...args: any[]) => any {
 	return function () {
 		try {
@@ -33,62 +59,131 @@ function tryCommand(callback: (...args: any[]) => any): (...args: any[]) => any 
 		}
 	}
 }
-
-const DEFAULT_WORD_REGEX = /\w+/
-const EXTENSION_NAME = "Custom Postfix Completion"
-let configuration: vscode.WorkspaceConfiguration;
-let languagePostfixTemplatesMap: Map<string, LanguagePostfixTemplate>;
-type LanguagePostfixTemplate = {
-	triggerWord: string;
-	description: string;
-	exprRegExp: RegExp;
-	body: string[];
-}
-
-function refreshConfigs() {
-	configuration = vscode.workspace.getConfiguration('custom-postfix-completion');
-	if (!configuration) {
-		return;
+function debugLog(message?: any, ...optionalParams: any[]): void {
+	if (isDebugMode) {
+		console.log(message, ...optionalParams);
 	}
-	let languageTemplatesRaw = configuration.get('languageTemplates')
-	if (!languageTemplatesRaw) {
-		return;
-	}
-	let languageTemplates = languageTemplatesRaw as { [key: string]: any }
-	const languageIds = Object.keys(languageTemplatesRaw);
-
-	let newMap = new Map();
-	languageIds.forEach((languageId) => {
-		let eachLangTemplate = languageTemplates[languageId] as { templates: LanguagePostfixTemplate[] | undefined; };
-		if (!eachLangTemplate.templates) {
-			return
-		}
-		eachLangTemplate.templates.forEach((template) => {
-			const key = getKey(languageId, template.triggerWord);
-			if (newMap.has(key)) {
-				showErrorMessage(`Duplicate triggerWord: ${template.triggerWord}`);
-				return
-			}
-
-			if (template.exprRegExp) {
-				console.log("exprRegExp", template.exprRegExp);
-				template.exprRegExp = new RegExp(template.exprRegExp);
-			} else {
-				template.exprRegExp = DEFAULT_WORD_REGEX
-			}
-			newMap.set(key, template);
-		})
-	});
-	languagePostfixTemplatesMap = newMap;
-	console.log("languagePostfixTemplatesMap", JSON.stringify(Array.from(languagePostfixTemplatesMap.entries())));
 }
-
 function showErrorMessage<T extends string>(message: string, ...items: T[]): Thenable<T | undefined> {
 	return vscode.window.showErrorMessage(`${EXTENSION_NAME}: ` + message, ...items);
 }
 
 function getKey(language: string, triggerWord: string): string {
 	return language + ':' + triggerWord;
+}
+function refreshConfigs() {
+	configuration = vscode.workspace.getConfiguration('custom-postfix-completion');
+	if (!configuration) {
+		return;
+	}
+	if (extensionContext.extensionMode !== vscode.ExtensionMode.Production
+		|| configuration.get<boolean>('debugMode')) {
+		isDebugMode = true;
+	} else {
+		isDebugMode = false;
+	}
+
+	parseLanguagePostfixTemplates();
+}
+function parseLanguagePostfixTemplates() {
+	let languageTemplatesRaw = configuration.get('languageTemplates')
+	if (!languageTemplatesRaw) {
+		return;
+	}
+	let languageTemplates = languageTemplatesRaw as { [key: string]: any }
+
+	let newMap = new Map();
+	const languageIds = Object.keys(languageTemplatesRaw);
+	for (const languageId of languageIds) {
+		let eachLangTemplate = languageTemplates[languageId] as { templates: LanguagePostfixTemplate[] | undefined; };
+		if (!eachLangTemplate.templates) {
+			continue;
+		}
+		for (const template of eachLangTemplate.templates) {
+			const key = getKey(languageId, template.triggerWord);
+			if (newMap.has(key)) {
+				showErrorMessage(`Duplicate template: ${template.triggerWord}`);
+				continue;
+			}
+
+			let validateMsg: string | undefined;
+			if ((validateMsg = validateAndParseTemplate(template)) !== undefined) {
+				showErrorMessage(`Invalid template: ${template.triggerWord}: ${validateMsg}`);
+				continue;
+			}
+
+			if (template.exprRegExp) {
+				template.exprRegExp = new RegExp(template.exprRegExp);
+			} else {
+				template.exprRegExp = DEFAULT_WORD_REGEX;
+			}
+			newMap.set(key, template);
+		}
+	}
+	languagePostfixTemplatesMap = newMap;
+	debugLog("languagePostfixTemplatesMap", JSON.stringify(Array.from(languagePostfixTemplatesMap.entries())));
+}
+/**
+ * Validates and parses a template.
+ *
+ * @param {LanguagePostfixTemplate} template - The template to validate and parse.
+ * @return {string | undefined} - Validation errors or undefined if the template is valid.
+ */
+function validateAndParseTemplate(template: LanguagePostfixTemplate): string | undefined {
+	let lastIndex = 0;
+	let parsedBody: (string | TemplateVarible)[] = [];
+	let matches: RegExpExecArray | null;
+
+	const body = template.body.join('\n');
+	while ((matches = POSSIBLE_VARIABLE_REGEX.exec(body)) !== null) {
+		if (matches.index > lastIndex) {
+			parsedBody.push(body.substring(lastIndex, matches.index));
+		}
+		lastIndex = matches.index + matches[0].length;
+
+		const possibleVariable = matches[0];
+		matches = VARIABLE_REGEX.exec(possibleVariable);
+		VARIABLE_REGEX.lastIndex = 0;
+		if (!matches) {
+			return `Wrong format of variable: ${possibleVariable}`;
+		}
+
+		const [variable, name, no, expression, defaultValue] = matches;
+		debugLog("variable", variable, "name", name, "no", no, "expression", expression, "defaultValue", defaultValue);
+
+		// 不定义 NO 的变量跳过用户交互
+		const skipUserInteraction = no === undefined;
+		const isExpr = /^expr$/i.test(name);
+		// 跳过用户交互的变量必须是 expr 变量，或者包含 EXPRESSION 或 DEFAULT_VALUE
+		if (skipUserInteraction && !isExpr && !expression && !defaultValue) {
+			return `NAME without #NO must include EXPRESSION or DEFAULT_VALUE: ${variable}`;
+		}
+		let expressionAndParam: (((word: string) => string) | string)[] | undefined;
+		if (expression) {
+			expressionAndParam = extractExpression(expression);
+			if (!expressionAndParam) {
+				return `Wrong format of expression: ${expression}`;
+			}
+			let expressionFunc = getExpressionFunc(expressionAndParam[0] as string);
+			if (!expressionFunc) {
+				return `Unsupported expression: ${expression}`;
+			}
+			expressionAndParam[0] = expressionFunc;
+		}
+
+		parsedBody.push({
+			name,
+			no: no ? Number(no) : undefined,
+			expressionAndParam: expressionAndParam,
+			defaultValue,
+		} as TemplateVarible);
+	}
+	POSSIBLE_VARIABLE_REGEX.lastIndex = 0;
+	if (lastIndex < body.length) {
+		parsedBody.push(body.substring(lastIndex));
+	}
+	template.parsedBody = parsedBody;
+	return undefined;
 }
 
 function applyTemplate() {
@@ -100,13 +195,13 @@ function applyTemplate() {
 	// 获取触发词范围
 	const cursorPosition = editor.selection.end;
 	const triggerWordRange = editor.document.getWordRangeAtPosition(cursorPosition, DEFAULT_WORD_REGEX);
-	console.log("triggerWordRange", triggerWordRange);
+	debugLog("triggerWordRange", triggerWordRange);
 	if (!triggerWordRange) {
 		return;
 	}
 	// 检查触发词前面是否 . 字符
 	let dotPosition = triggerWordRange.start.translate(0, -1);
-	console.log("dotPosition", dotPosition);
+	debugLog("dotPosition", dotPosition);
 	let dotStr = editor.document.getText(new vscode.Range(dotPosition, dotPosition.translate(0, 1)));
 	if (!dotStr || dotStr !== '.') {
 		return
@@ -122,107 +217,68 @@ function applyTemplate() {
 
 	// 获取模板应用于的表达式（也就是.前面的那部分）
 	const exprRange = editor.document.getWordRangeAtPosition(dotPosition/* .translate(0, -1) */, template.exprRegExp);
-	console.log("exprRange", exprRange);
+	debugLog("exprRange", exprRange);
 	if (!exprRange) {
 		return;
 	}
 	let exprWord = editor.document.getText(exprRange);
-	console.log("triggerWord", triggerWord, "exprWord", exprWord);
+	debugLog("triggerWord", triggerWord, "exprWord", exprWord);
 	if (!triggerWord || !exprWord) {
 		return;
 	}
 	if (exprWord.endsWith('.' + triggerWord)) {
 		exprWord = exprWord.substring(0, exprWord.length - triggerWord.length - 1)
 	}
-	console.log("triggerWord", triggerWord, "exprWord", exprWord);
+	debugLog("triggerWord", triggerWord, "exprWord", exprWord);
 
-	const snippet = templateToSnippet(exprWord, template);
+	const snippet = templateToSnippet(template, exprWord);
 	if (snippet) {
 		let replaceRange = new vscode.Range(exprRange.start, triggerWordRange.end);
 		editor.insertSnippet(snippet, replaceRange);
 	}
 }
-
-function templateToSnippet(exprWord: string, template: LanguagePostfixTemplate): vscode.SnippetString | undefined {
-	let body = template.body.join('\n');
-	if (body === '') {
-		return new vscode.SnippetString(body);
-	}
-
-	let snippet = transformToSnippetFormat(body, exprWord);
-	if (!snippet) {
-		return undefined;
-	}
-	console.log("snippet", snippet);
-	return new vscode.SnippetString(snippet);
-}
-
-function transformToSnippetFormat(body: string, exprWord: string): string | undefined {
-	const variablePattern = /\$\{(\w+)(?:#(\d+))?:?(\w+\(expr\))?:?([^\}]+)?\}/g;
-	let validationErrors: string[] = [];
-	let transformedBody = body;
-	let matches: RegExpExecArray | null;
-
-	while ((matches = variablePattern.exec(body)) !== null) {
-		const [variable, name, no, expression, defaultValue] = matches;
-		console.log("variable", variable, "name", name, "no", no, "expression", expression, "defaultValue", defaultValue);
-
-		// Validate NAME
-		if (!/^(\w+)$/.test(name)) {
-			validationErrors.push(`Invalid NAME in variable: ${variable}`);
-		}
-		// Validate #NO
-		if (no !== undefined && !/^\d+$/.test(no)) {
-			validationErrors.push(`Invalid #NO in variable: ${variable}`);
-		}
-		// Validate EXPRESSION
-		if (expression !== undefined && !/^\w+\(expr\)$/.test(expression)) {
-			validationErrors.push(`Invalid EXPRESSION in variable: ${variable}`);
+function templateToSnippet(template: LanguagePostfixTemplate, exprWord: string): vscode.SnippetString | undefined {
+	const snippet = new vscode.SnippetString();
+	for (const part of template.parsedBody) {
+		if (typeof part === 'string') {
+			snippet.appendText(part);
+			continue;
 		}
 
-		// 不定义 NO 的变量跳过用户交互
-		const isSkipUserInteraction = no === undefined;
-		const isExpr = /^expr$/i.test(name);
-		// 跳过用户交互的变量必须包含 EXPRESSION 或 DEFAULT_VALUE，除非是 expr 变量
-		if (isSkipUserInteraction && !isExpr && !expression && !defaultValue) {
-			validationErrors.push(`NAME without #NO must include EXPRESSION or DEFAULT_VALUE: ${variable}`);
-		}
-
-		if (validationErrors.length === 0) {
-			// Transform the variable
-			let newValue = isExpr ? exprWord : evalExpression(expression, exprWord) || defaultValue || '';
-			if (isSkipUserInteraction) {
-				// Replace with EXPRESSION or DEFAULT_VALUE
-				transformedBody = transformedBody.replace(variable, newValue);
-			} else {
-				// Replace with placeholder format
-				transformedBody = transformedBody.replace(variable, `\${${no}:${newValue}}`);
-			}
+		let variable = part as TemplateVarible;
+		const skipUserInteraction = variable.no === undefined;
+		const isExpr = /^expr$/i.test(variable.name);
+		const value = isExpr ? exprWord : evalExpression(variable.expressionAndParam, exprWord) || variable.defaultValue || '';
+		if (skipUserInteraction) {
+			snippet.appendText(value);
 		} else {
-			// Output validation errors and return undefined
-			console.error('Validation errors:', validationErrors.join('; '));
+			snippet.appendPlaceholder(value, variable.no);
+		}
+	}
+
+	debugLog("snippet", snippet);
+	return snippet;
+}
+function extractExpression(expression: string): string[] | undefined {
+	let match = /^(\w+)\((.*)\)$/.exec(expression);
+	return match ? [match[1], match[2]] : undefined;
+}
+function getExpressionFunc(expressionName: string): undefined | ((expr: string) => string) {
+	switch (expressionName) {
+		case "escapeString": {
+			return expressionEscapeString;
+		}
+		default: {
 			return undefined;
 		}
 	}
-
-	return transformedBody;
 }
-
-function evalExpression(expression: string | undefined, exprWord: string): string | undefined {
-	if (expression) {
-		let expressionName = expression.substring(0, expression.indexOf('('));
-		switch (expressionName) {
-			case "escapeString": {
-				return expressionEscapeString(exprWord);
-			}
-			default: {
-				showErrorMessage("Unsupported expression: " + expressionName);
-				return expression;
-			}
-		}
+function evalExpression(expressionAndParam: (((word: string) => string) | string)[] | undefined, exprWord: string): string | undefined {
+	if (!expressionAndParam) {
+		return undefined;
 	}
+	return (expressionAndParam[0] as ((expr: string) => string))(exprWord);
 }
-
 function expressionEscapeString(exprWord: string): string {
 	return exprWord.replaceAll('"', '\\"');
 }
